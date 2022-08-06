bd2d8ae0c8a533904b30768bd2a19127a1c5a8aa
Flameberry <flameberrydev@gmail.com>
2022-08-06 11:38:32 +0530

    Removed CMakeLists.txt from submodule 'imgui'

--- Dependencies.cmake
+++ Dependencies.cmake
@@ -1,5 +1,5 @@
 # OpenGL Helper Libs
-set(GRAPHICS_LIBS glfw Glad imgui)
+set(GRAPHICS_LIBS glfw Glad)
 
 if(APPLE)
     # Inbuilt mac frameworks required for GLFW
@@ -20,4 +20,14 @@ set(GRAPHICS_INCLUDE_DIRS
     ${SOURCE_DIR}/vendor/GLFW/include
     ${SOURCE_DIR}/vendor/Glad/include
     ${SOURCE_DIR}/vendor/glm
+)
+
+# ImGui files
+file(GLOB IMGUI_SRC ${SOURCE_DIR}/vendor/imgui/*.cpp ${SOURCE_DIR}/vendor/imgui/*.h)
+list(APPEND IMGUI_SRC
+    ${SOURCE_DIR}/vendor/imgui/backends/imgui_impl_glfw.cpp
+    ${SOURCE_DIR}/vendor/imgui/backends/imgui_impl_glfw.h
+    ${SOURCE_DIR}/vendor/imgui/backends/imgui_impl_opengl3.cpp
+    ${SOURCE_DIR}/vendor/imgui/backends/imgui_impl_opengl3.h
+    ${SOURCE_DIR}/vendor/imgui/backends/imgui_impl_opengl3_loader.h
 )
